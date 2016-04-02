2ae77954c38b5fb7349d39acb3270a6e975e3d51
Jonathan <sunyang713@hotmail.com>
2016-04-02 03:26:45 -0400

    Delete comments. Noisy.

--- liberated/responses.py
+++ liberated/responses.py
@@ -36,49 +36,11 @@ def index():
         names.append(result['name'])  # can also be accessed using result[0]
     cursor.close()
 
-    #
-    # Flask uses Jinja templates, which is an extension to HTML where you can
-    # pass data to a template and dynamically generate HTML based on the data
-    # (you can think of it as simple PHP)
-    # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-    #
-    # You can see an example template in templates/index.html
-    #
-    # context are the variables that are passed to the template.
-    # for example, 'data' key in the context variable defined below will be
-    # accessible as a variable in index.html:
-    #
-    #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-    #     <div>{{data}}</div>
-    #     
-    #     # creates a <div> tag for each element in data
-    #     # will print: 
-    #     #
-    #     #   <div>grace hopper</div>
-    #     #   <div>alan turing</div>
-    #     #   <div>ada lovelace</div>
-    #     #
-    #     {% for n in data %}
-    #     <div>{{n}}</div>
-    #     {% endfor %}
-    #
     context = dict(data = names)
 
-    #
-    # render_template looks in the templates/ folder for files.
-    # for example, the below file reads template/index.html
-    #
     return render_template('index.html', **context)
 
 
-#
-# This is an example of a different path.  You can see it at:
-# 
-#     localhost:4111/another
-#
-# Notice that the function name is another() rather than index()
-# The functions for each app.route need to have different names
-#
 @app.route('/another')
 def another():
     return render_template('another.html')
