b12f4e70a94785be723c4998ee1bbe1fb583e8a4
Jonathan <sunyang713@hotmail.com>
2016-04-02 03:06:52 -0400

    Cleanup in __init__.

--- liberated/__init__.py
+++ liberated/__init__.py
@@ -1,28 +1,20 @@
-import os
-from sqlalchemy import *
-from sqlalchemy.pool import NullPool
-from flask import Flask, g, Response
-import yaml
-from liberated.configureEngine import configureEngine
 # from make_json_app import make_json_app
 # app = make_json_app(__name__)
 # tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-# app = Flask(__name__.split('.')[0], template_folder=tmpl_dir) # @1st-param: from flask docs b/c this is a package now.
+# app = Flask(__name__.split('.')[0], template_folder=tmpl_dir)
+import os
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
+import yaml
+
+
+from flask import Flask, g, Response
 app = Flask(__name__.split('.')[0])
 import liberated.responses
 
+
+from liberated.configureEngine import configureEngine
 engine = configureEngine()
-#
-# Example of running queries in your database
-# Note that this will probably not work if you already have a table named 'test' in your database,
-# containing meaningful data. This is only an example showing you how to run queries in your
-# database using SQLAlchemy.
-#
-engine.execute("""CREATE TABLE IF NOT EXISTS test (
-    id serial,
-    name text
-);""")
-engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
 
 
 @app.before_request
